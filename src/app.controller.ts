27c97476b16907e4866074fd0cfa3e6e82255ff2
Erikson Gonçalves Sanchez <eriksongoncalves@yahoo.com.br>
2024-11-28 18:10:51 -0300

    feat: config prisma and improviments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/users')
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async getUsers(): Promise<any[]> {
+    return await this.prisma.user.findMany()
   }
 }
